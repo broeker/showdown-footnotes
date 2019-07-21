b89f160d87c5e985243548066cf532280ce17e2b
broeker <tim@electriccitizen.com>
2019-07-20 21:22:51 -0500

    SD-00: patch to support backlinks.

--- dist/index.js
+++ dist/index.js
@@ -8,14 +8,15 @@ module.exports = function () {
     filter: function filter(text) {
       return text.replace(/^\[\^([\d\w]+)\]:\s*((\n+(\s{2,4}|\t).+)+)$/mg, function (str, name, rawContent, _, padding) {
         var content = converter.makeHtml(rawContent.replace(new RegExp('^' + padding, 'gm'), ''));
-        return '<div class="footnote" id="footnote-' + name + '"><a href="#footnote-' + name + '"><sup>[' + name + ']</sup></a>:' + content + '</div>';
+        return '<div class="footnote" id="footnote-' + name + '"><a id="backlink-' + name + '" href="#footnote-' + name + '"><sup>[' + name + ']</sup></a>:' + content + '</div>';
       });
     }
   }, {
     type: 'lang',
     filter: function filter(text) {
       return text.replace(/^\[\^([\d\w]+)\]:( |\n)((.+\n)*.+)$/mg, function (str, name, _, content) {
-        return '<small class="footnote" id="footnote-' + name + '"><a href="#footnote-' + name + '"><sup>[' + name + ']</sup></a>: ' + content + '</small>';
+        return '<small class="footnote" id="footnote-' + name + '"><a href="#footnote-' + name + '"><sup>[' + name + ']</sup></a>: ' + content + '<a href="backlink-' + name + '">^</a></small>';
+
       });
     }
   }, {
